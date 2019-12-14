28136e58880d188be0187b6386411d4fa5a09861
Henrique Gregory <henriquegre45@gmail.com>
2019-12-14 16:27:14 -0300

    Adiciona suporte a -h

--- usuarios.sh
+++ usuarios.sh
@@ -6,6 +6,22 @@
 #
 # José Henrique <henriquegreg45@gmail.com>
 # data de criação 13/12/2019
-# Versão: 0.1
+#
+# Versão: 0.1 Mostra usuários e nomes separados por TAB
+# Versão: 0.2 Adicionado suporte à opção -h
 
+MSG_USO="
+
+    Uso: $0 [-h]
+    -h      Mostra essa tela de ajuda e sai.
+
+"
+# Tratamento das opções de linha de comando.
+if test "$1" = "-h"
+then
+    echo "$MSG_USO"
+    exit 0
+fi
+
+# Processamento
 cut -d : -f1,5 /etc/passwd | tr : \\t
