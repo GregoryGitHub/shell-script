1b75449217127b24e97f4985e10a03885bb8b8b8
Henrique Greg <jose.henrique.silva@accenture.com>
2019-12-20 08:51:10 -0300

    Adiciona as opções -s e --sort

--- usuarios.sh
+++ usuarios.sh
@@ -4,23 +4,29 @@
 # Mostra os logins e nomes de usuários do sistema
 # Obs. Lê dados do arquivo /etc/passwd
 #
-# José Henrique <henriquegreg45@gmail.com>
-# data de criação 13/12/2019
-#
+# ------------- Resumo das versões -----------------------------
 # Versão 1: Mostra usuários e nomes separados por TAB
 # Versão 2: Adicionado suporte à opção -h
 # Versão 3: Adicionado suporte à opção -V e opções inválidas
 # Versão 4: Arrumado bug quando não tem opções, basename no
-#       nome do programa, -V extraindo direto dos cabeçlhos,
-#       adicionadas opções --help e --version
+#           nome do programa, -V extraindo direto dos cabeçlhos,
+#           adicionadas opções --help e --version
+# Versão 5: Adiciona as opções -s e --sort para ordenar a 
+#           saída.
+#
+#
+# José Henrique <henriquegreg45@gmail.com>
+# data de criação 13/12/2019
+
+ordenar=0 # A saída deve ser ordenada de forma alfabética?
 
 MSG_USO="
 
-    Uso: $(basename $0) [-h|-V]
+    Uso: $(basename $0) [-h|-V|-s]
 
         -h,--help        Mostra essa tela de ajuda e sai.
-        
         -V,--version     Mostra a versão do programa e sai.
+        -s,--sort        Ordena a saída alfabeticamente.
 
 "
 # Tratamento das opções de linha de comando.
@@ -38,6 +44,10 @@ case "$1" in
 
     exit 0
 ;;
+-s|--sort)
+    # Altera o valor de ordenar
+    ordenar=1
+;;
 *)
     if test -n "$1"
     then
@@ -49,4 +59,11 @@ case "$1" in
 esac
 
 # Processamento
-cut -d : -f1,5 /etc/passwd | tr : \\t
+# Extrai a listagem
+lista=$(cut -d : -f1,5 /etc/passwd)
+
+if test "$ordenar" = 1
+then
+    lista=$(echo "$lista" | sort)
+fi
+ echo "$lista" | tr : \\t
